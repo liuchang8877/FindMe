2ba21a8971efe7eb3d631f410b9e57da7df07eae
liuchang <liuchang8877@gmail.com>
2013-04-15 18:33:31 +0800

    Revert "add the login"
    This reverts commit 48fe45896ba630b6b96a1988b2ec77de7b5059e9.

---
+++ LBSFind/LSViewController.h
@@ -0,0 +1,22 @@
+//
+//  LSViewController.h
+//  LoginSample
+//
+//  Created by Joe Cyboski on 7/12/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class LSUser;
+
+@interface LSViewController : UIViewController
+
+// Properties to hold authenticated user
+@property (retain, nonatomic) IBOutlet UILabel *usernameLabel;
+@property (retain, nonatomic) LSUser *user;
+
+
+- (IBAction)logoutPressed:(id)sender;
+
+@end
