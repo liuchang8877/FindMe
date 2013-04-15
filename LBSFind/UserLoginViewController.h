78a7caa4bca52cd3d3ac5ae1a285b95ea65a1599
liuchang <liuchang8877@gmail.com>
2013-04-15 18:35:52 +0800

    Revert "Revert "add the login""
    This reverts commit b754af75455ac1c6c8294429b80c39992821c23c.

---
+++ LBSFind/UserLoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  UserLoginViewController.h
+//  LBSFind
+//
+//  Created by liu on 4/15/13.
+//  Copyright (c) 2013 liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UserLoginViewController : UIViewController
+
+@end
--- LBSFind/tool.m
+++ LBSFind/tool.m
@@ -8,6 +8,10 @@
 
 #import "tool.h"
 #import "Reachability.h"
+#import "allConfig.h"
+#import "DataModel.h"
+#import "ASIHTTPRequest.h"
+#import "SBJson.h"
 
 @implementation tool
 
@@ -40,6 +44,63 @@
 		return NO;
 	}
 	return YES;
+}
+
+#pragma mark http for map info
++ (void)setTheLocation:(userLocation *)myLocation
+{
+    //[SVProgressHUD showWithStatus:@"数据加载中..." maskType:SVProgressHUDMaskTypeGradient];
+    
+    NSString *urlStr = [NSString stringWithFormat:@"http://%@:%@/test/lbs/do.php?action=sendloc&tel=%@&longi=%@&lati=%@",URL_IP,URL_PORT,myLocation.tel,myLocation.x,myLocation.y];
+    NSURL *url = [NSURL URLWithString:urlStr];
+
+    ASIHTTPRequest *myRequest;
+    NSLog(@"setTheLocation---url:%@",url);
+    if ([self checkNet])
+    {
+        NSLog(@"setTheLocation---net is ok");
+        [myRequest setDelegate:nil];
+        [myRequest cancel];
+        myRequest = [ASIHTTPRequest requestWithURL:url];
+        [myRequest setDelegate:self];
+        [myRequest setDidFailSelector:@selector(userLocationRequestFinished:)];
+        [myRequest setDidFinishSelector:@selector(userLocationRequestFailed:)];
+        [myRequest startAsynchronous];
+        
+        
+    } else {
+        
+        NSLog(@"setTheLocation---net is NOT ok");
+        [self checkNetAlter];
+    }
+}
+
++ (void)userLocationRequestFinished:(ASIHTTPRequest *)request
+{
+    // Use when fetching text data
+    //NSString *responseString = [request responseString];
+    
+    // Use when fetching binary data
+    //NSData *responseData = [request responseData];
+    
+    NSString *responseString=[[NSString alloc] initWithData:[request responseData] encoding:NSUTF8StringEncoding];
+    
+    NSLog(@"userLocationRequestFinished---responseString:%@",responseString);
+    
+//    SBJsonParser * parser = [[SBJsonParser alloc] init];
+//    NSError * error = nil;
+//    NSMutableDictionary *jsonDic = [parser objectWithString:responseString error:&error];
+//    
+//    NSMutableDictionary * dicRetInfo = [jsonDic objectForKey:@"ret"];
+
+}
+
+
++ (void)userLocationRequestFailed:(ASIHTTPRequest *)request
+{
+    NSError *error = [request error];
+    NSLog(@"userLocationRequestFailed---error:%@",error);
+    [self checkNetAlter];
 }
 
 
